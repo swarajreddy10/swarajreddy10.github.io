47e759a5b649a71bd47c6f7ff18e4260e566bf57
swarajreddy10 <swarajchandra22@gmail.com>
2025-09-22 14:18:30 +0530

    Updates

---
+++ _next/static/chunks/app/layout-cbabadf62d43b957.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},1307:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1290,23))}},e=>{e.O(0,[741,441,255,358],()=>e(e.s=1307)),_N_E=e.O()}]);
